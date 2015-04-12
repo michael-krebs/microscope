55925fff11bda1ceb042cc62f395b21600358cd9
Michael Krebs <vault742@gmail.com>
2015-04-11 23:45:48 -0400

    more error reporting

--- client/templates/includes/errors.js
+++ client/templates/includes/errors.js
@@ -3,3 +3,11 @@ Template.errors.helpers({
 		return Errors.find();
 	}
 });
+
+// Remove errors from collection after 3 seconds (when they are finished displaying)
+Template.error.rendered = function () {
+	var error = this.data;
+	Meteor.setTimeout(function() {
+		Errors.remove(error._id);
+	}, 3000);
+};
--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,16 @@
+Template.postSubmit.created = function () {
+	Session.set('postSubmitErrors', {});
+}
+
+Template.postSubmit.helpers({
+	errorMessage: function (field) {
+		return Session.get('postSubmitErrors')[field];
+	},
+	errorClass: function (field) {
+		return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+	}
+});
+
 Template.postSubmit.events({
 	'submit form': function (e) {
 		e.preventDefault();
@@ -10,11 +23,11 @@ Template.postSubmit.events({
 		Meteor.call('postInsert', post, function(error, result) {
 			// display the error to the user and abort
 			if (error)
-				return alert(error.reason);
+				return throwError(error.reason);
 
 			// show this result but route anyways
 			if (result.postExists)
-				alert('This link has already been posted.');
+				throwError('This link has already been posted.');
 			
 			Router.go('postPage', {_id: result._id});
 		});
