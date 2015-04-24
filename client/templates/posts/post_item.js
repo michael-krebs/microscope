aaf124f973afe67184cfc949e65a7f60c81ef525
Michael Krebs <vault742@gmail.com>
2015-04-24 15:50:46 -0400

    began adding upvoting

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -4,9 +4,24 @@ Template.postItem.helpers({
 		return this.userId === Meteor.userId();
 	},
 
-	domain: function () {
+	domain: function() {
 		var a = document.createElement('a');
 		a.href = this.url;
 		return a.hostname;
+	},
+	upvotedClass: function() {
+		var userId = Meteor.userId();
+		if (userId && !_.include(this.upvoters, userId)) {
+			return 'btn-primary upvotable';
+		} else {
+			return 'disabled';
+		}
 	}
 });
+
+Template.postItem.events({
+	'click .upvotable': function(e) {
+		e.preventDefault();
+		Meteor.call('upvote', this._id);
+	}
+})
